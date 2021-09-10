dc9b79ce950ad891ce432bbc3474051ccf45077f
samwel-chege <samwelchegeh09@gmail.com>
2021-09-10 23:49:20 +0300

    create image and profile model classes

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Profile(models.Model):
+    photo  = models.ImageField(upload_to='photos')
+    bio = models.TextField(max_length=1000)
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='photos')
+    name= models.CharField(max_length=30)
+    caption = models.CharField(max_length=30)
+    profile = models.ForeignKey(Profile, on_delete=CASCADE)
+    likes = models.IntegerField(default=0)
+    comments = models.TextField(max_length=100)
