4116cddf436112bf2922cca6fb2c1bcbdcb2ed62
samwel-chege <samwelchegeh09@gmail.com>
2021-09-11 23:59:23 +0300

    create Comments model class

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,3 +15,8 @@ class Image(models.Model):
     profile = models.ForeignKey(Profile, on_delete=CASCADE)
     likes = models.IntegerField(default=0)
     comments = models.TextField(max_length=100)
+
+class Comments(models.Model):
+    content  = models.TextField()
+    posted_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=CASCADE)   
