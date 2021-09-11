fbae3f9d60bb6ec9b3e6ec3391629e274f19ce3c
samwel-chege <samwelchegeh09@gmail.com>
2021-09-11 21:36:42 +0300

    query data from the image table

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,8 @@
+from instagram.models import Image
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    images = Image.objects.all()
+    return render(request,'index.html',{"images": images})
+
