41a18c283ca1b42948916f5e294def233cfe2ee6
samwel-chege <samwelchegeh09@gmail.com>
2021-09-12 21:32:12 +0300

    define a like_photo view function

--- instagram/views.py
+++ instagram/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
-from instagram.models import Image, Profile
-from django.shortcuts import render
+from django.db.models.signals import post_save
+from django.http.response import Http404, HttpResponseRedirect
+from instagram.models import Comments, Image, Profile
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
     images = Image.objects.all()
-
-    return render(request,'index.html',{"images": images})
+    comments = Comments.objects.all()
+    comments_count = comments.count()
+    return render(request,'index.html',{"images": images,"comments":comments})
 
 
 def profile(request,id):
@@ -25,3 +29,30 @@ def profile(request,id):
             new_user_profile.followers.add(request.user)#add my profile to their followers
             new_user_profile.save()
     return render(request,'profile.html',locals())
+
+def comment(request,image_id):
+    image = Image.objects.get(pk=image_id)
+    content = request.GET.get("comment")
+    print(content)   
+    user = request.user
+    comment = Comments(image = image,content = content,user = user)
+    comment.save_comment() 
+    return redirect('home')
+
+
+def like_photo(request,image_id):
+    image = Image.objects.get(pk=image_id)
+    liked = False
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(user = current_user)
+    except  Profile.DoesNotExist:
+        raise Http404
+    if image.likes.filter(id=profile.id).exists():
+        image.likes.remove(profile)
+        liked = False
+    else:
+        image.likes.add(profile)
+        liked = True
+    return HttpResponseRedirect(reverse('home'))    
+
