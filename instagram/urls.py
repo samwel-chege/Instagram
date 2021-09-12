c030e6ce1792cb6f9be9e71565dcf5172b0dcbcd
samwel-chege <samwelchegeh09@gmail.com>
2021-09-12 16:25:52 +0300

    edit urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from . import views
+from . import views 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('profile/<int:id>',views.profile,name = 'profile')
     
 ]
 
