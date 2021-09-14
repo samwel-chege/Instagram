aaa21df47c4e039502026274f176d826e20f9d42
samwel-chege <samwelchegeh09@gmail.com>
2021-09-14 18:52:40 +0300

    update urls

--- insta/urls.py
+++ insta/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('instagram.urls')),
+    path('logout/', views.LogoutView.as_view()),  
     path('accounts/', include('registration.backends.simple.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
