1e31625638597a12651532f6a14b2ecb06db1e9b
samwel-chege <samwelchegeh09@gmail.com>
2021-09-14 13:28:20 +0300

    add redirect paths

--- insta/settings.py
+++ insta/settings.py
@@ -138,3 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = "/accounts/login/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
